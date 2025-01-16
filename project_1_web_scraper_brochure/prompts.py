1d17e36b9453a3094dcc3a4c18595ace820649af
1HongyeXiong <hongye.xiong@outlook.com>
2025-01-15 17:32:28 -0800

    Add the system prompt for the content generation function

--- project_1_web_scraper_brochure/prompts.py
+++ project_1_web_scraper_brochure/prompts.py
@@ -6,3 +6,4 @@ def get_links_user_prompt(website):
     user_prompt += "\n".join(website.links)
     return user_prompt
 
+system_prompt = "You are an assistant that analyzes the contents of several relevant pages from a company website and creates a short brochure about the company for prospective customers, investors, and recruits. Respond in markdown."
